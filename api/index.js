8f8d9406dc42d4a60c1925a52e6325a35444d023
Praks <aim.prakhar@gmail.com>
2023-03-27 21:11:46 +0530

    gg

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import authRoute from "./routes/auth.js"
 import hotelsRoute from "./routes/hotels.js"
 import roomsRoute from "./routes/rooms.js"
 import usersRoute from "./routes/users.js"
-import cookieParser from "cookie-parser";
+// import cookieParser from "cookie-parser";
 import cors from "cors";
 
  const app=express();
@@ -33,7 +33,7 @@ const connect =async()=>{
 
 //middlewares
 app.use(cors());
-app.use(cookieParser())
+// app.use(cookieParser())
 app.use(express.json());
 app.use("/api/auth",authRoute);
 app.use("/api/users",usersRoute);
